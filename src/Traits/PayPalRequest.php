5a4dc64f905b46bdeb01fc50420d3eae41f599fd
benq <benq7879@gmail.com>
2016-09-12 13:11:48 +0800

    Fix missing import class

--- src/Traits/PayPalRequest.php
+++ src/Traits/PayPalRequest.php
@@ -4,6 +4,7 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Exception\BadResponseException;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\ServerException;
+use Srmklive\PayPal\Services\AdaptivePayments;
 
 trait PayPalRequest
 {
